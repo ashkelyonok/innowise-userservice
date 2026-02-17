c7d3e4126f710d38a3da1ee439cbc4cd05e4530f
Anastasia Shkelyonok <anastasia.shkelyonok@gmail.com>
2026-02-18 02:23:40 +0300

    refactor exceptions

--- src/main/java/org/ashkelyonok/userservice/exception/MaxCardsLimitException.java
+++ src/main/java/org/ashkelyonok/userservice/exception/MaxCardsLimitException.java
@@ -1,8 +1,21 @@
 package org.ashkelyonok.userservice.exception;
 
+import java.io.Serial;
+
 public class MaxCardsLimitException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1120072459229959037L;
 
     public MaxCardsLimitException() {
         super("User already has maximum (5) active cards");
+    }
+
+    public MaxCardsLimitException(String message) {
+        super(message);
+    }
+
+    public MaxCardsLimitException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
